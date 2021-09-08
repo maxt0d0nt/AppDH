e046301dd0ff6f5a76f7c1fc0ca4b01c2856a776
maxt0d0nt <maxbox21@hotmail.com>
2021-09-08 15:48:48 -0300

    modularice toda la primera parte

--- app.js
+++ app.js
@@ -1,25 +1,2 @@
-let fs = require ("fs");
-let tareas = fs.readFileSync ("./tareas.json"); // llama al archivo json, OJO UTF-8
+let modulo = require("./funcionesDeTareas");
 
-let arrayTareas = JSON.parse(tareas); //lo guarda en una variable como un array, si usamos utf-8 antes no es necesario usar esto
-
-//console.log(arrayTareas);
-
-//console.log (process.argv)
-
-switch(process.argv[2]){
-    case "listar":
-        for (i= 0; i<arrayTareas.length; i++){
-            console.log(arrayTareas[i]);
-        }
-     console.log ("=====================");
-        break;
-
-    case undefined:
-        console.log("Atencion! - Tienes que pasar una accion!");
-        break;
-
-    default:
-        console.log("No entiendo que quisiste decir");
-
-}
---
+++ funcionesDeTareas.js
@@ -0,0 +1,27 @@
+let fs = require ("fs");
+let tareas = fs.readFileSync ("./tareas.json"); // llama al archivo json, OJO UTF-8
+
+let arrayTareas = JSON.parse(tareas); //lo guarda en una variable como un array, si usamos utf-8 antes no es necesario usar esto
+
+let suiche = function abc(){
+
+    switch(process.argv[2]){
+        case "listar":
+            for (i= 0; i<arrayTareas.length; i++){
+                console.log(arrayTareas[i]);
+            }
+         console.log ("=====================");
+            break;
+    
+        case undefined:
+            console.log("Atencion! - Tienes que pasar una accion!");
+            break;
+    
+        default:
+            console.log("No entiendo que quisiste decir");
+    
+    }
+    }
+ suiche();
+   
+module.exports = suiche;
